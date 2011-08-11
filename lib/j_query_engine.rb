e593e4fd20422cf762e457148ec3f1660e4ab429
Matthias Folz <folz@avarteq.de>
2011-08-11 19:59:00 +0200

    fix error in initial module

--- lib/j_query_engine.rb
+++ lib/j_query_engine.rb
@@ -1,6 +1,6 @@
 module JQueryEngine
   Root         = File.expand_path('../..', __FILE__)
-  RelativeRoot = Root.gsub(Rails.root, '')
+  RelativeRoot = Root.gsub(Rails.root.to_s, '')
   Javascript   = File.join(RelativeRoot, 'public', 'javascript')
   require "#{Root}/lib/j_query_engine/engine"
 
